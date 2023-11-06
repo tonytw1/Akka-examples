d3110da0e5a0e70010085e3ec1349bd74155bf8a
Tony McCrae <tony.mccrae@theguardian.com>
2023-11-06 11:13:43 +0000

    Out actor can pass a message to another of out actors when they share the same actorSystem.

--- src/main/scala/com/example/AkkaQuickstart.scala
+++ src/main/scala/com/example/AkkaQuickstart.scala
@@ -1,23 +1,34 @@
 //#full-example
 package com.example
 
-
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-class MyActor extends Actor {
-  override def receive: Receive = {
-    case m =>
-      println("Received: " + m)
-  }
-}
-
 
 //#main-class
 object AkkaQuickstart extends App {
   val actorSystem = ActorSystem()
 
+  class MyActor extends Actor {
+    private val anotherActorRef: ActorRef = actorSystem.actorOf(Props[AnotherActor])
+
+    override def receive: Receive = {
+      case m =>
+        println("MyActor Received: " + m)
+        anotherActorRef ! "Relay " + m
+    }
+  }
+
+  class AnotherActor extends Actor {
+    override def receive: Receive = {
+      case m =>
+        println("AnotherActor received: " + m)
+    }
+  }
+
   private val myActorRef: ActorRef = actorSystem.actorOf(Props[MyActor], name = "myActor")
+  private val anotherActorRef: ActorRef = actorSystem.actorOf(Props[AnotherActor], name = "anotherActor")
+
   myActorRef ! "Hello world"
+  anotherActorRef ! "Hello world"
 
   println("Done")
 }
-
